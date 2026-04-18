b31607614ff7220e6336c8636977dd710cc19ebc
adiweb12 <vsadith051@gmail.com>
2026-04-18 08:24:48 +0530

    Change hilt-work dependency version to 1.2.0
    Downgrade hilt-work dependency version from 1.2.1 to 1.2.0.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -80,7 +80,7 @@ dependencies {
     implementation("com.google.dagger:hilt-android:2.51.1")
     kapt("com.google.dagger:hilt-android-compiler:2.51.1")
     implementation("androidx.hilt:hilt-navigation-compose:1.2.0")
-    implementation("androidx.hilt:hilt-work:1.2.1")
+    implementation("androidx.hilt:hilt-work:1.2.0")
     kapt("androidx.hilt:hilt-compiler:1.2.1")
 
     // ── Room DB ───────────────────────────────────────────────────────────
