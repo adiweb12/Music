133b2d0f255646bd0f522d70cfa38c3b005a5721
adiweb12 <vsadith051@gmail.com>
2026-04-19 07:23:24 +0530

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -67,7 +67,7 @@ dependencies {
     implementation("com.google.dagger:hilt-android:2.51.1")
     kapt("com.google.dagger:hilt-android-compiler:2.51.1")
     implementation("androidx.hilt:hilt-navigation-compose:1.2.0")
-    kapt("androidx.hilt:hilt-compiler:1.2.1")
+    kapt("androidx.hilt:hilt-compiler:1.2.0")
 
     // Room
     val roomVersion = "2.6.1"
